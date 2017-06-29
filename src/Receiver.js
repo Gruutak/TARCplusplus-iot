89d5aa85524d33cfd5c63155b87cb83c35ddcffe
Gruutak <lucasbertz@hotmail.com>
2017-06-29 22:42:20 +0000

    Converted receiver into a class.

--- src/Receiver.js
+++ src/Receiver.js
@@ -1,37 +1,38 @@
 'use strict';
 import nconf from 'nconf';
-
-//https://docs.microsoft.com/pt-br/azure/iot-hub/iot-hub-node-node-direct-methods
-
 import {Client as EventHubClient} from 'azure-event-hubs';
 
-//const connectionString = 'HostName=AqueleHubLa.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=QkrX/R/Z6N4lVXDaLo4eCSq0xGYgr8NUPuRLLFRj3Qc=';
-const connectionString = `HostName=${nconf.get('HOSTNAME')};SharedAccessKeyName=${nconf.get('HUB_ACCESS_KEY_NAME')};SharedAccessKey=${nconf.get('HUB_ACCESS_KEY')}`;
+export class Receiver {
+	constructor() {
+		const connectionString = `HostName=${nconf.get('HOSTNAME')};SharedAccessKeyName=${nconf.get('HUB_ACCESS_KEY_NAME')};SharedAccessKey=${nconf.get('HUB_ACCESS_KEY')}`;
 
+		this.client = EventHubClient.fromConnectionString(connectionString);
+	}
 
-function printError(err) {
-	console.log(err.message);
-};
+	printError(err) {
+		console.log(err.message);
+	}
 
-function printMessage(message) {
-	console.log('Message received: ');
-	console.log(JSON.stringify(message.body));
-	console.log('');
-};
+	printMessage(message) {
+		console.log('Message received: ');
+		console.log(JSON.stringify(message.body));
+		console.log('');
+	}
 
-let client = EventHubClient.fromConnectionString(connectionString);
-
-export function openClient() {
-	client.open()
-	.then(client.getPartitionIds.bind(client))
-    .then(function (partitionIds) {
-        return partitionIds.map(function (partitionId) {
-            return client.createReceiver('$Default', partitionId, { 'startAfterTime' : Date.now()}).then(function(receiver) {
-                console.log(`Created partition receiver: ${partitionId}`)
-                receiver.on('errorReceived', printError);
-                receiver.on('message', printMessage);
-            });
-        });
-    })
-    .catch(printError);
+	run() {
+		let that = this;
+		let client = this.client;
+		client.open()
+		.then(client.getPartitionIds.bind(client))
+	    .then(function (partitionIds) {
+	        return partitionIds.map(function (partitionId) {
+	            return client.createReceiver('$Default', partitionId, { 'startAfterTime' : Date.now()}).then(function(receiver) {
+	                console.log(`Created partition receiver: ${partitionId}`)
+	                receiver.on('errorReceived', that.printError);
+	                receiver.on('message', that.printMessage);
+	            });
+	        });
+	    })
+	    .catch(this.printError);
+	}
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './setup-config';
-import { openClient as runReceiver } from './Receiver';
+import {Receiver} from './Receiver';
 import { openClient as runDevice } from './Device';
 
 
-// runReceiver();
-runDevice();
+let receiver = new Receiver();
+receiver.run();
+// runDevice();
