9948bf6eea0bd346c2bb0ce398d67769bbb56269
Gruutak <lucasbertz@hotmail.com>
2017-07-13 18:49:12 +0000

    Added twitter support.

--- src/Receiver.js
+++ src/Receiver.js
@@ -2,12 +2,28 @@
 import nconf from 'nconf';
 import { Client as EventHubClient } from 'azure-event-hubs';
 import logger from './logger';
+import Twitter from 'twitter';
 
 export class Receiver {
 	constructor() {
 		const connectionString = `HostName=${nconf.get('HOSTNAME')};SharedAccessKeyName=${nconf.get('HUB_ACCESS_KEY_NAME')};SharedAccessKey=${nconf.get('HUB_ACCESS_KEY')}`;
 
 		this.client = EventHubClient.fromConnectionString(connectionString);
+
+		this.twitter_client = new Twitter({
+			consumer_key: nconf.get('TWITTER_CONSUMER_KEY'),
+			consumer_secret: nconf.get('TWITTER_CONSUMER_SECRET'),
+			access_token_key: nconf.get('TWITTER_ACCESS_TOKEN_KEY'),
+			access_token_secret: nconf.get('TWITTER_ACCESS_TOKEN_SECRET')
+		});
+
+		var params = {screen_name: 'nodejs'};
+
+		this.twitter_client.get('statuses/user_timeline', params, function(error, tweets, response) {
+			if (!error) {
+				console.log(tweets);
+			}
+		});
 	}
 
 	printError(err) {
