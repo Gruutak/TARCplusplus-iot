50ff27e335f92b7c50335f9aec2381e3ce3b8b3e
Gruutak <lucasbertz@hotmail.com>
2017-07-13 13:02:04 +0000

    Reading python output

--- src/Device.js
+++ src/Device.js
@@ -3,7 +3,7 @@ import nconf from 'nconf';
 import { clientFromConnectionString } from 'azure-iot-device-mqtt';
 import { Message } from 'azure-iot-device';
 import logger from './logger';
-import mraa from 'mraa';
+import spawn from 'child_proccess';
 
 /*
 	PINS for mraa:
@@ -20,15 +20,6 @@ export class Device {
 		const connectionString = `HostName=${nconf.get('HOSTNAME')};DeviceId=${nconf.get('DEVICE_ID')};SharedAccessKey=${nconf.get('DEVICE_ACCESS_KEY')}`;
 
 		this.client = clientFromConnectionString(connectionString);
-
-		// //do something when app is closing
-		// process.on('exit', this.exitHandler);
-		//
-		// //catches ctrl+c event
-		// process.on('SIGINT', this.exitHandler);
-		//
-		// //catches uncaught exceptions
-		// process.on('uncaughtException', this.exitHandler);
 	}
 
 	printResultFor(op) {
@@ -38,42 +29,19 @@ export class Device {
 		};
 	}
 
-	// exitHandler() {
-	// 	this.spi.close();
-	//
-	// 	process.exit();
-	// }
-
 	run() {
-		// Working with SPI for analog sensors
-		console.log(`Iniciando leitura`);
-		let spiTemp = new mraa.Spi(0);
-		let temperature_buffer = new Buffer(3);
-		temperature_buffer[0] = 0x01;
-		temperature_buffer[1] = 0xA0;
-		temperature_buffer[2] = 0x00;
+		logger.info(`Iniciando leitura`);
 
+		py = spawn(`sudo python3`, [`sensors.py`]);
 
-		setInterval(() => {
-			let buf2 = spiTemp.write(temperature_buffer);
-			console.log("Sent: " + temperature_buffer.toString('hex') + ". Received: " + buf2.toString('hex'));
-		}, 1000)
+		py.stdout.on(`data`, data => {
+			logger.warn(data);
+		});
 
-		// Working with MRAA and digital sensors
-		// console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
-		//
-		// let touchSensor = new mraa.Gpio(25); //setup digital read on pin 25
-		// touchSensor.dir(mraa.DIR_IN); //set the gpio direction to input
-		//
-		// let led = new mraa.Gpio(23); //setup digital read on pin 23
-		// led.dir(mraa.DIR_OUT);
-		//
-		// function periodicActivity() {
-    	// 		let touch_value = touchSensor.read(); //read the digital value of the pin
-		// 		led.write(touch_value);
-		// }
-		//
-		// setInterval(periodicActivity, 2000); //call the indicated function every 0.001 second (1000 milliseconds)
+		ls.stderr.on('data', function (data) {
+			logger.error('stderr: ' + data);
+		});
+
 
 		/*
 		let client = this.client;
